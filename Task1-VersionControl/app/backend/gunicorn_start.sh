52b1a42b97ae6397acb12b95f85eb275169e4fad
joelclaudius <otiiclaudius@gmail.com>
2025-01-31 13:01:05 +0000

    mod

--- Task1-VersionControl/app/backend/gunicorn_start.sh
+++ Task1-VersionControl/app/backend/gunicorn_start.sh
@@ -3,5 +3,8 @@
 # Activate virtual environment
 source /app/venv/bin/activate
 
+# Run migrations
+/app/venv/bin/python /app/manage.py migrate
+
 # Start Gunicorn server
 exec gunicorn -b 0.0.0.0:8000 -w 3 backend.wsgi:application
